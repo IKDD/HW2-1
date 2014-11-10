6f409d6fea1a789ca85d4e5ee23c84b2226e0a5d
HankLyu <hanklgs9564@gmail.com>
2014-11-10 13:40:03 +0800

    finish the input remain output table

--- IKDDhw2.py
+++ IKDDhw2.py
@@ -4,7 +4,10 @@ This homework is to scan query string and parse twitter data
 Then print out matching text, its user_id, user_name
 '''
 import psycopg2
+import sys
+#from tabulate import tabulate
 
+#set the server database information
 config = {
 	'user' : 'hanklgs9564_user_4527',
 	'password' : 'RnoM3V7M',
@@ -13,9 +16,26 @@ config = {
 	'dbname' : 'hanklgs9564_db_4527'
 }
 
-conn = psycopg2.connect(**config)
+query = raw_input()
+SQLstring = "SELECT text, user_name, user_id FROM twitter WHERE q = \'" + query + "\' ORDER BY user_id"
+
+#connect iServDB
+try:
+	conn = psycopg2.connect(**config)
+except:
+	print "Unable to connect to the database."
 cursor = conn.cursor()
-cursor.execute("SELECT text, user_name, user_id FROM twitter WHERE q = 'haha' ORDER BY user_id")
-print(cursor.fetchall())
+try:
+	cursor.execute(SQLstring)
+except:
+	print "Database error!."
+
+result = cursor.fetchall()
+print (result)
+
+print "///////////////////////////////////"
+for i in result:
+	print i[0],",,,,,,,,,,,,",i[1],",,,,,,,,,,",i[2]
+
 conn.close()
 cursor.close()
